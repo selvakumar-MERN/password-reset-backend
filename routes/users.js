e74b9487c8326a4528aeb88b2796fa5cf7b6972e
selvakumar k <84513538+selvakumar-MERN@users.noreply.github.com>
2023-07-14 11:36:45 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,8 @@ router.post('/register',userData.emailExist);
 router.post('/verify',passwordReset.emailverify)
 router.post('/resetpassword/:token',passwordReset.resetpassword);
 router.post('/verifyuser/:token',passwordReset.verifytoken);
+router.post('/login',userData.emaillogin)
+router.post('verifylogin',userData.verifylogin)
 
 
 module.exports=router;
